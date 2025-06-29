3e9e66a79a54791b62c14f47be579f799f7fb83a
AryaShrestha05 <aryashrestha005@gmail.com>
2025-06-29 18:37:23 -0400

    Initial file, imported gsap, scrolltrigger, splittext. Installed and kept tailwindcss in all files.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import gsap from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+//These gsap plugins arent automatically active so you have to register them. you only need to do this once
+import { ScrollTrigger} from "gsap/all";
+
+//Makes sure both plugins are used globally for every file to use
+//splittext splits texts and individually moves it
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-indigo-300">Hello</h1>
+        </div>
+    )
+}
+
+export default App;
