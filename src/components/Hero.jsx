ef31562c567197d182f5711a265856dfa77b3684
AryaShrestha05 <aryashrestha005@gmail.com>
2025-07-04 15:14:30 -0400

    Created the final Hero page with all GSAP properties

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,21 +10,50 @@ const Hero = () => {
         const heroSplit = new SplitText('.title', { type:'chars, words' })
         //I want to target the subtitle class and split it to only lines.
         const paragraphSplit = new SplitText('.subtitle', { type:'lines' })
-
+        
         heroSplit.chars.forEach((char) => char.classList.add('text-gradient'))
 
         gsap.from(heroSplit.chars, {
             yPercent: 100,
+            opacity: 0,
             duration: 1.8,
             ease: 'expo.out',
             stagger: 0.06,
+        });
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 3,
+            ease: 'expo.inOut',
+            stagger: 0.06,
+            delay: 0.7,
+        });
+        gsap.from('.mandala', {
+            yPercent: -100,
+            opacity: 0,
+            duration: 5,
+            ease: 'expo.inOut',
+            stagger: 0.06,
+            delay: 1,
+        });
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                //Animation animates according to scroll
+                scrub: true,
+            }
         })
+        .to('.right-leaf', { y: 200 }, 0)
+        .to('.left-leaf', { y: -200 }, 0)
+
     }, [])
     
     return (
         <>
             <section id ="hero" className="noisy">
-                <h1 className = "title">Vivi </h1>
+                <h1 className = "title">MOJITO</h1>
                 <img
                 src='/images/hero-left-leaf.png'
                 alt='left leaf'
@@ -39,14 +68,14 @@ const Hero = () => {
                 <div className = "body">
                     <div className = "content">
                         <div className = "space-y-5 hidden md:block">
-                            <p>Mandala Street, Thamel</p>
+                            <p className='mandala'>Mandala Street, Thamel</p>
                             <p className="subtitle">Sip the Sprit <br /> of Summer</p>
                         </div>
 
                         <div className='view-cocktails'>
                             <p className = "subtitle">Unforgettable soulful vibes you won’t find anywhere else in Kathmandu!
                             </p>
-                            <a href = '#cocktails'>View Drinks</a>
+                            <a className='mandala' href = '#cocktails'>View Drinks</a>
                         </div>    
                     </div>
                 </div>
