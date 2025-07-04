4e478c0dab7786e0c30d0d0ba590d9eb17be3e80
AryaShrestha05 <aryashrestha005@gmail.com>
2025-07-04 18:11:28 -0400

    Minor changes to wording

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
             <div>
                 <a href="#home" className={"flex items-center gap-2"}>
                     <img src="/images/logo.png" alt="logo of velvet pour, the company" />
-                    <p>Velvet Pour</p>
+                    <p>Arya Shrestha</p>
                 </a>
 
                  <ul>
